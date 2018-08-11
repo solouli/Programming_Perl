5080c7c9e73341abd6fdcc9974df5ce0177de631
Marisol <solnava.mi@gmail.com>
2018-08-10 20:59:13 -0500

    the usual hello world and some notes

---
+++ hola_mundo.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+# <print> es una función (instrucción o comando)
+# imprime " Hola mundo " con vueltas de carro
+
+print "\n Hola mundo \n\n";
+
+# <for> es una función que dentro que tiene argumentos
+# imprime Hola mundo 11 veces con una sola vuelta de carro
+
+for ($i = 0; $i <= 10; $i++) {
+print "Hola mundo $i\n";
+}
+
+# imprime Hola mundo 11 veces con una sola vueltas de carro 
+
+for ($i = 0; $i <= 10; $i++) {
+print "\n Hola mundo \n\n";
+}
---
+++ session01.pl
@@ -0,0 +1,43 @@
+
+            Introducción a la Programación usando el lenguaje Perl
+
+######################### Viernes 10 de Agosto de 2018 #########################
+
+Arturo Ocádiz R.
+Unidad de Cómputo (IBt)
+arturo@ibt.unam.mx
+
+> Técnicas de solución de problemas
+
+* Parte analítica
+
+sintáxis: orden
+semántica: significado
+ide
+
+* Parte creativa
+
+error de lógica: el programa corre pero no hace lo que tu quieres
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> hola mundo
+
+Problema: El granjero, el zorro, el pato y el maíz
+
+> Herramientas o técnicas para solución de problemas
+
+a) planeación
+b) replantear el problemas
+c) dividir el problema
+d) iniciar con lo que ya se conoce
+e) reducir el problema
+f) buscar analogías
+g) experimentar
+h) evitar frustrarse
+
+################################### Tareas: ###################################
+
+1) Resolver ejercicio 9 y 10 de:
+https://blockly-games.appspot.com/maze?lang=en&level=10&skin=0
+
+2) En tus propias palabras describe cómo resuelves un rompecabezas de 8 fichas
+  y 9 lugares
